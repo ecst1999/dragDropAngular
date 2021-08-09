42d3d7094cdf79767fdb610f7d185eb4f3c7cace
Steve Carvajal <exactas1999@gmail.com>
2021-08-09 16:01:18 -0500

    Subir ejercicio de practica de Drag Angular

---
+++ src/app/virtual/virtual.component.ts
@@ -0,0 +1,36 @@
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual',
+  templateUrl: './virtual.component.html',
+  styleUrls: ['./virtual.component.css']
+})
+export class VirtualComponent implements OnInit {
+
+  @ViewChild( CdkVirtualScrollViewport ) viewPort: CdkVirtualScrollViewport;
+
+  personas = Array(1000).fill(0);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  irFinal(){
+
+    this.viewPort.scrollToIndex( this.personas.length );
+
+  }
+
+  irInicio(){
+
+    this.viewPort.scrollToIndex( 0 );
+
+  }
+
+  irMitad(){
+    this.viewPort.scrollToIndex( this.personas.length/2 );
+  }
+
+}
